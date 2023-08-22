747f4f51660bf5ddaf1ed66770dd02c8dcdea44d
ambientWave <ambientWave@online.dev>
2023-08-22 21:33:06 +0400

    database initialization and migration

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Notes(models.Model):
+    body = models.TextField(null=True, blank=True)
+    update_date = models.DateTimeField(auto_now=True) # activated on each sql update
+    create_date = models.DateTimeField(auto_now_add=True) # activated only at the time of creation (sql insertion)
+
+    def __str__(self):
+        return self.body[0:50]
+    
