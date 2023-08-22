33877c7e0fd4ffba7207984a9631fbce4e1be1ca
ambientWave <ambientWave@online.dev>
2023-08-22 21:14:54 +0400

    routes of api endpoints

--- api/views.py
+++ api/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def getRoutes(request):
+
+    routes = [
+    {
+        'Endpoint': '/notes/',
+        'method': 'GET',
+        'body': None,
+        'description': 'Returns an array of notes'
+    },
+    {
+        'Endpoint': '/notes/id',
+        'method': 'GET',
+        'body': None,
+        'description': 'Returns a single note object'
+    },
+    {
+        'Endpoint': '/notes/create/',
+        'method': 'POST',
+        'body': {'body': ""},
+        'description': 'Creates new note with data sent in post request'
+    },
+    {
+        'Endpoint': '/notes/id/update/',
+        'method': 'PUT',
+        'body': {'body': ""},
+        'description': 'Creates an existing note with data sent in post request'
+    },
+    {
+        'Endpoint': '/notes/id/delete/',
+        'method': 'DELETE',
+        'body': None,
+        'description': 'Deletes and exiting note'
+    },
+    ]
+
+    return JsonResponse(routes, safe=False)
--- notes_app/settings.py
+++ notes_app/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.AppConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
